dd8234282b01743d79807bf9e5fd31591d614a4b
Xinxia <985025074@qq.com>
2025-06-04 16:49:54 +0800

    first

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+import simpleaudio as sa
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+
+def timer() -> None:
+    print("Hello from timer!")
+    times = 0
+    interval = input("please input interval that reminds you(minutes): ")
+    while True:
+        time.sleep(int(interval) * 60)
+        times = times + 1
+        # play alert
+        obj = AudioSegment.from_mp3("./alert.mp3")
+        play(obj)
+
+        print(f"It's time to take a break!,you have finished {times} times!")
+
+
+def main() -> None:
+    timer()
+if __name__ == "__main__":
+    timer()
